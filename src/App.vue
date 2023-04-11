076cc62d9d0e8e015a3d58cf035d4b07d0d2ebea
zhangbo <15686056663@163.com>
2023-04-11 10:13:55 +0800

    and design vue 安装和配置

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,14 @@
-<script setup lang="ts"></script>
-
 <template>
-  <div id="app"></div>
+  <div id="app">
+    <AConfigProvider :locale="zhCN">
+      <AButton>Hello AnddUI</AButton>
+      <AInput></AInput>
+      <ADatePicker />
+    </AConfigProvider>
+  </div>
 </template>
+<script setup lang="ts">
+import zhCN from 'ant-design-vue/es/locale/zh_CN'
+</script>
 
 <style scoped></style>
