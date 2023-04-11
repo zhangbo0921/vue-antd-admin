6bf8d90a6ce24b54d2d515f95a46e9f090cf3f14
zhangbo <15686056663@163.com>
2023-04-11 10:26:45 +0800

    Pinia

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,6 +1,15 @@
 <template>
-  <div>{{ title }}</div>
+  <div>
+    {{ title }}
+    <AButton @click="click">{{ counter.count }}</AButton>
+  </div>
 </template>
 <script setup lang="ts">
+import { useCounterStore } from '@/stores'
+const counter = useCounterStore()
+
+const click = () => {
+  counter.increment()
+}
 const title = 'home page'
 </script>
